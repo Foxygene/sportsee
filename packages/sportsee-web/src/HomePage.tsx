13a1119193c9cb64b913c58ec3f921943ea7a443
Foxygene <foxygene.dev@gmail.com>
2023-10-07 02:05:49 +0200

    feat(HomePage): added fast previews access for all users

--- packages/sportsee-web/src/HomePage.tsx
+++ packages/sportsee-web/src/HomePage.tsx
@@ -1,5 +1,7 @@
 import SideNavbar from "./components/side-navbar/SideNavbar";
 import TopNavbar from "./components/top-navbar/TopNavbar";
+import users from "./assets/Mocks/mockMainData.json";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
@@ -7,22 +9,15 @@ function HomePage() {
       <TopNavbar />
       <SideNavbar />
       <main>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-        dignissimos, amet porro fugit explicabo id corporis? Excepturi ducimus
-        est ipsa enim voluptatem impedit exercitationem velit! Ipsam quibusdam
-        eaque architecto quia tempora laborum est! Fugiat quaerat quibusdam
-        totam temporibus distinctio cum necessitatibus, at quis nesciunt
-        reprehenderit soluta sed autem voluptatem ut nihil dolores inventore
-        sapiente non laborum beatae ipsum esse ex. Velit ab tempora
-        perspiciatis, voluptate, voluptas, ea pariatur enim sunt id molestias
-        repudiandae maiores eos labore aliquam quidem eligendi necessitatibus
-        commodi reiciendis? Temporibus nostrum nihil quidem aperiam autem,
-        facilis mollitia dolorum natus? Assumenda, deserunt ipsa distinctio
-        eligendi blanditiis necessitatibus dolorem, odio nisi rerum eveniet
-        neque. Recusandae atque alias at, dolor minus officiis in officia
-        obcaecati quasi aliquam eligendi tempore labore iusto exercitationem
-        incidunt commodi unde, perferendis nisi, placeat beatae esse corporis
-        sint! Ea hic necessitatibus harum veniam amet optio consequuntur
+        <ul>
+          {users.map((user) => (
+            <li>
+              <Link to={`/user/${user.id}`}>
+                {user.userInfos.firstName} {user.userInfos.lastName}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </main>
     </>
   );
--- packages/sportsee-web/src/components/top-navbar/TopNavbar.tsx
+++ packages/sportsee-web/src/components/top-navbar/TopNavbar.tsx
@@ -1,11 +1,12 @@
 import "./topNavbar.css";
 import logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
 
 function TopNavbar() {
   return (
     <div className="top-navbar">
       <img src={logo} />
-      <a>Acceuil</a>
+      <Link to="/">Acceuil</Link>
       <a>Profil</a>
       <a>Réglages</a>
       <a>Communauté</a>
