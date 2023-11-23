34a6eaefeac2f063c4f2d147435794724c8c80a3
Foxygene <foxygene.dev@gmail.com>
2023-11-23 19:49:04 +0100

    feat(data): some mock setup (WIP)

--- packages/sportsee-web/src/data/info.tsx
+++ packages/sportsee-web/src/data/info.tsx
@@ -1,7 +1,33 @@
-async function getInfo(userID: string) {
-  const response = await fetch(`http://localhost:3000/user/${userID}`);
-  const data = await response.json();
-  console.log(data);
+import mockActivity from "../assets/Mocks/mockActivity.json";
+// import mockAverageSession from "../assets/Mocks/mockAverageSessions.json";
+// import mockMainData from "../assets/Mocks/mockMainData.json";
+// import mockPerformance from "../assets/Mocks/mockPerformance.json";
+
+function getActivityData(useMock: boolean, userID: number) {
+  if (useMock) {
+    return mockActivity.filter((user) => user.userId === userID);
+  }
+  //utiliser terner quand on fetch
 }
 
-export default getInfo;
+// async function getAverageSessionData(useMock: boolean, userID: number) {}
+
+// if (type === "AverageSession") {
+//   if (useMock)
+//     return mockAverageSession.filter((user) => user.userId === userID);
+// }
+
+// if (type === "MainData") {
+//   if (useMock) return mockMainData.filter((user) => user.id === userID);
+// }
+
+// if (type === "Performance") {
+//   if (useMock)
+//     return mockPerformance.filter((user) => user.userId === userID);
+// }
+
+// const response = await fetch(`http://localhost:3000/user/${userID}`);
+// const data = await response.json();
+// console.log(data);
+
+export default getActivityData;
