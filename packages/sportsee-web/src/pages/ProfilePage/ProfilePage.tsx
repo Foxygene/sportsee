736c968e5b95e2c25b4fd5ef913988ac44189151
Foxygene <foxygene.dev@gmail.com>
2023-11-23 19:49:47 +0100

    feat(Profile): Barchart Base / Style WIP

--- packages/sportsee-web/src/pages/ProfilePage/ProfilePage.tsx
+++ packages/sportsee-web/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,10 +1,43 @@
 import SideNavbar from "../../components/side-navbar/SideNavbar";
 import TopNavbar from "../../components/top-navbar/TopNavbar";
+import getActivityData from "../../data/info";
 import { userMainData } from "../../types/userMainData";
 import "./profilePage.css";
+import {
+  Bar,
+  BarChart,
+  CartesianAxis,
+  CartesianGrid,
+  Legend,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 function ProfilePage(prop: userMainData) {
-  console.log(prop);
+  const activity = getActivityData(true, prop.data.id)![0];
+
+  const barChartData = activity.sessions.map((session) => {
+    return {
+      day: session.day.substring(9),
+      Kilogrammes: session.kilogram,
+      Calories: session.calories,
+    };
+  });
+
+  const kgMinMaxValues = [
+    Math.min(
+      ...barChartData.map((session) => {
+        return session.Kilogrammes;
+      })
+    ),
+    Math.max(
+      ...barChartData.map((session) => {
+        return session.Kilogrammes;
+      })
+    ),
+  ];
+
   return (
     <>
       <TopNavbar />
@@ -16,6 +49,46 @@ function ProfilePage(prop: userMainData) {
             {prop.data.userInfos.firstName}
           </span>
         </div>
+        <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+        <BarChart
+          width={835}
+          height={320}
+          data={barChartData}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+          barGap={10}
+          barCategoryGap={35}
+        >
+          <CartesianGrid strokeDasharray="3 3" vertical={false} />
+          <XAxis dataKey="day" />
+          <YAxis
+            domain={[kgMinMaxValues[0] - 1, kgMinMaxValues[1] + 1]}
+            orientation="right"
+            axisLine={false}
+            tickLine={false}
+            yAxisId={"Kilo"}
+            tickCount={4}
+          />
+          <YAxis yAxisId={"Cal"} style={{ display: "none" }} />
+          <Tooltip />
+          <Legend />
+          <Bar
+            dataKey="Kilogrammes"
+            fill="#000000"
+            yAxisId={"Kilo"}
+            radius={[10, 10, 0, 0]}
+          />
+          <Bar
+            dataKey="Calories"
+            fill="#FF0101"
+            yAxisId={"Cal"}
+            radius={[10, 10, 0, 0]}
+          />
+        </BarChart>
       </main>
     </>
   );
