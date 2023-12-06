2c2f9e082fb7209db41d5d37a6140495cef7f4f4
Foxygene <foxygene.dev@gmail.com>
2023-12-06 01:41:29 +0100

    feat(barchart): added correct data to custom tooltip

--- packages/sportsee-web/src/components/graphs/BarChartSetup.tsx
+++ packages/sportsee-web/src/components/graphs/BarChartSetup.tsx
@@ -7,13 +7,34 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
+import "./barChartSetup.css";
 
 function BarChartSetup(props: {
   barChartData: { day: string; Kilogrammes: number; Calories: number }[];
   kgMinMaxValues: number[];
 }) {
+  function CustomTooltip(active: any): JSX.Element | null {
+    const payloadIsEmpty = active.payload.length;
+
+    if (!payloadIsEmpty) {
+      return null;
+    }
+
+    return (
+      <div className="tool-tip__bar-chart">
+        <p className="tool-tip__bar-chart-text">
+          {`${active.payload[0].payload.Kilogrammes}kg`}
+        </p>
+        <p className="tool-tip__bar-chart-text">
+          {" "}
+          {`${active.payload[0].payload.Calories}Kcal`}
+        </p>
+      </div>
+    );
+  }
+
   return (
-    <ResponsiveContainer width="100%" height={185}>
+    <ResponsiveContainer className={"barchart"} width="100%" height={185}>
       <BarChart
         data={props.barChartData}
         margin={{
@@ -22,11 +43,9 @@ function BarChartSetup(props: {
           left: 20,
           bottom: 5,
         }}
-        // barGap={10}
-        // barCategoryGap={35}
       >
         <CartesianGrid strokeDasharray="3 3" vertical={false} />
-        <XAxis dataKey="day" />
+        <XAxis dataKey="day" tickMargin={20} />
         <YAxis
           domain={[props.kgMinMaxValues[0] - 1, props.kgMinMaxValues[1] + 1]}
           orientation="right"
@@ -34,20 +53,23 @@ function BarChartSetup(props: {
           tickLine={false}
           yAxisId={"Kilo"}
           tickCount={4}
+          tickMargin={43}
         />
         <YAxis yAxisId={"Cal"} style={{ display: "none" }} />
-        <Tooltip />
+        <Tooltip content={<CustomTooltip />} />
         <Bar
           dataKey="Kilogrammes"
           fill="#000000"
           yAxisId={"Kilo"}
           radius={[10, 10, 0, 0]}
+          barSize={7}
         />
         <Bar
           dataKey="Calories"
           fill="#FF0101"
           yAxisId={"Cal"}
           radius={[10, 10, 0, 0]}
+          barSize={7}
         />
       </BarChart>
     </ResponsiveContainer>
