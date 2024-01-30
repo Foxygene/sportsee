091761173753b1476447fc169e0a6292f412d181
Foxygene <foxygene.dev@gmail.com>
2024-01-30 21:00:58 +0100

    feat(data): now can gather average session duration

--- packages/sportsee-web/src/data/info.tsx
+++ packages/sportsee-web/src/data/info.tsx
@@ -1,13 +1,19 @@
 import mockActivity from "../assets/Mocks/mockActivity.json";
-// import mockAverageSession from "../assets/Mocks/mockAverageSessions.json";
+import mockAverageSession from "../assets/Mocks/mockAverageSessions.json";
 // import mockMainData from "../assets/Mocks/mockMainData.json";
 // import mockPerformance from "../assets/Mocks/mockPerformance.json";
 
-function getActivityData(useMock: boolean, userID: number) {
+export function getActivityData(useMock: boolean, userID: number) {
   if (useMock) {
     return mockActivity.filter((user) => user.userId === userID);
   }
   //utiliser terner quand on fetch
+}
+
+export function getAverageSessionData(useMock: boolean, userID: number) {
+  if (useMock) {
+    return mockAverageSession.filter((user) => user.userId === userID);
+  }
 }
 
 // async function getAverageSessionData(useMock: boolean, userID: number) {}
@@ -29,5 +35,3 @@ function getActivityData(useMock: boolean, userID: number) {
 // const response = await fetch(`http://localhost:3000/user/${userID}`);
 // const data = await response.json();
 // console.log(data);
-
-export default getActivityData;
