8ebc52e967ab4e5c7759331bf67afdc76ce8e78b
Foxygene <foxygene.dev@gmail.com>
2024-05-24 14:15:16 +0200

    chore(useDataSwitch): added typing to context provider

--- packages/sportsee-web/src/hooks/useDataSwitch.tsx
+++ packages/sportsee-web/src/hooks/useDataSwitch.tsx
@@ -1,11 +1,15 @@
-import { createContext, useState } from "react";
+import { ReactNode, createContext, useState } from "react";
+
+interface Props {
+  children?: ReactNode;
+}
 
 export const dataSwitchContext = createContext({
   dataSwitchValue: "api",
   dataSwitch: () => {},
 });
 
-export function DataSwitchContextProvider({ children }) {
+export function DataSwitchContextProvider({ children }: Props) {
   const [dataSwitchValue, setDataSwitchValue] = useState("mock");
 
   const dataSwitch = () => {
