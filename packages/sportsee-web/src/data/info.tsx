2c6d76e0094aed6cf82789b4f4faa248eea5d3c5
Foxygene <foxygene.dev@gmail.com>
2024-06-02 19:12:09 +0200

    feat(API info gathering): setup WIP

--- packages/sportsee-web/src/data/info.tsx
+++ packages/sportsee-web/src/data/info.tsx
@@ -3,23 +3,35 @@ import mockAverageSession from "../assets/Mocks/mockAverageSessions.json";
 // import mockMainData from "../assets/Mocks/mockMainData.json";
 import mockPerformance from "../assets/Mocks/mockPerformance.json";
 
+async function getFromAPI(userID: number, category: string) {
+  const response = await fetch(
+    `http://localhost:3000/user/${userID}${category}`
+  );
+  const data = await response.json();
+  return data;
+}
+
 export function getActivityData(isMock: boolean, userID: number) {
   if (isMock) {
     return mockActivity.filter((user) => user.userId === userID);
   }
+  getFromAPI(userID, "/activity");
 }
 
 export function getAverageSessionData(isMock: boolean, userID: number) {
   if (isMock) {
     return mockAverageSession.filter((user) => user.userId === userID);
   }
+  return getFromAPI(userID, "/average-sessions");
 }
 
 export function getPerformanceData(isMock: boolean, userID: number) {
   if (isMock) {
     return mockPerformance.filter((user) => user.userId === userID);
   }
+  return getFromAPI(userID, "/performance");
 }
+
 // const response = await fetch(`http://localhost:3000/user/${userID}`);
 // const data = await response.json();
 // console.log(data);
