acf69e156facc3c85deb56ad4fad0b23172094a6
Foxygene <foxygene.dev@gmail.com>
2024-06-02 19:10:55 +0200

    feat(radar chart): base setup

--- packages/sportsee-web/src/components/graphs/radar-chart-setup/RadarChartSetup.tsx
+++ packages/sportsee-web/src/components/graphs/radar-chart-setup/RadarChartSetup.tsx
@@ -8,22 +8,37 @@ import {
 } from "recharts";
 
 function RadarChartSetup(props) {
+  const customTicks = [
+    { value: 0, label: "Intensité" },
+    { value: 1, label: "Vitesse" },
+    { value: 2, label: "Force" },
+    { value: 3, label: "Endurance" },
+    { value: 4, label: "Energie" },
+    { value: 5, label: "Cardio" },
+  ];
+
   return (
-    <ResponsiveContainer width={258} height={263}>
+    <ResponsiveContainer width={340} height={300}>
       <RadarChart
+        width={258}
+        height={263}
         cx="50%"
         cy="50%"
         outerRadius="80%"
         data={props.RadarChartData}
       >
-        <PolarGrid />
-        <PolarAngleAxis dataKey="kind" />
-        <PolarRadiusAxis />
+        <PolarGrid gridType="polygon" radialLines={false} />
+        <PolarAngleAxis
+          tickFormatter={(value) =>
+            customTicks[value] && customTicks[value].label
+          }
+        />
+        <PolarRadiusAxis tick={false} axisLine={false} />
         <Radar
           dataKey="value"
-          stroke="#8884d8"
-          fill="#8884d8"
-          fillOpacity={0.6}
+          stroke="#FF0101"
+          fill="#FF0101"
+          fillOpacity={0.7}
         />
       </RadarChart>
     </ResponsiveContainer>
