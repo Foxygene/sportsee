f3f7836786a4848f6e23020d9a51b4ec89b4a565
Foxygene <foxygene.dev@gmail.com>
2024-06-09 19:28:18 +0200

    feat(useDataFetcher): full rework WIP

--- packages/sportsee-web/src/data/info.tsx
+++ packages/sportsee-web/src/data/info.tsx
@@ -1,37 +1,81 @@
-import mockActivity from "../assets/Mocks/mockActivity.json";
-import mockAverageSession from "../assets/Mocks/mockAverageSessions.json";
+import { useEffect, useState } from 'react';
+import mockActivity from '../assets/Mocks/mockActivity.json';
+import mockAverageSession from '../assets/Mocks/mockAverageSessions.json';
 // import mockMainData from "../assets/Mocks/mockMainData.json";
-import mockPerformance from "../assets/Mocks/mockPerformance.json";
+import mockPerformance from '../assets/Mocks/mockPerformance.json';
 
-async function getFromAPI(userID: number, category: string) {
-  const response = await fetch(
-    `http://localhost:3000/user/${userID}${category}`
-  );
-  const data = await response.json();
+interface ActivityData {
+  userId: number;
+  sessions: {
+    day: string;
+    kilogram: number;
+    calories: number;
+  }[];
+}
+
+interface AverageSessionData {
+  userId: number;
+  sessions: {
+    day: number;
+    sessionLength: number;
+  }[];
+}
+
+interface PerformanceData {
+  userId: number;
+  kind: {
+    '1': string;
+    '2': string;
+    '3': string;
+    '4': string;
+    '5': string;
+    '6': string;
+  };
+  data: {
+    value: number;
+    kind: number;
+  }[];
+}
+
+type Data = ActivityData | AverageSessionData | PerformanceData | null;
+
+export function useDataFetcher(
+  isMock: boolean,
+  userID: number,
+  category: string
+): Data {
+  const [data, setData] = useState<Data>(null);
+
+  useEffect(() => {
+    if (isMock) {
+      let mockData: Data | null = null;
+      if (category === '/activity') {
+        mockData = mockActivity.find((user) => user.userId === userID) || null;
+      } else if (category === '/average-sessions') {
+        mockData =
+          mockAverageSession.find((user) => user.userId === userID) || null;
+      } else if (category === '/performance') {
+        mockData =
+          mockPerformance.find((user) => user.userId === userID) || null;
+      }
+      setData(mockData);
+    } else {
+      const getData = async () => {
+        const apiUrl = `http://localhost:3000/user/${userID}${category}`;
+        try {
+          const response = await fetch(apiUrl);
+          if (!response.ok) {
+            throw new Error(`Did not get an ok. Got: ${response.statusText}`);
+          }
+          const json = await response.json();
+          setData(json as Data);
+        } catch (error) {
+          console.error(`Error getting data: woooops`);
+        }
+      };
+      getData();
+    }
+  }, [isMock, userID, category]);
+
   return data;
 }
-
-export function getActivityData(isMock: boolean, userID: number) {
-  if (isMock) {
-    return mockActivity.filter((user) => user.userId === userID);
-  }
-  getFromAPI(userID, "/activity");
-}
-
-export function getAverageSessionData(isMock: boolean, userID: number) {
-  if (isMock) {
-    return mockAverageSession.filter((user) => user.userId === userID);
-  }
-  return getFromAPI(userID, "/average-sessions");
-}
-
-export function getPerformanceData(isMock: boolean, userID: number) {
-  if (isMock) {
-    return mockPerformance.filter((user) => user.userId === userID);
-  }
-  return getFromAPI(userID, "/performance");
-}
-
-// const response = await fetch(`http://localhost:3000/user/${userID}`);
-// const data = await response.json();
-// console.log(data);
