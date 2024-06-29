5632935d7ea76fe330827d098694a48ec1b44b74
Foxygene <foxygene.dev@gmail.com>
2024-06-30 01:25:44 +0200

    refactor(Route): removed unnecessary code

--- packages/sportsee-web/src/route.tsx
+++ packages/sportsee-web/src/route.tsx
@@ -1,17 +1,14 @@
-import { createHashRouter } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import users from "./assets/Mocks/mockMainData.json";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
-
-const profilePage = users.map((profile) => ({
-  path: `/user/${profile.id}`,
-  element: <ProfilePage key={profile.id} data={profile} />,
-}));
+import { createHashRouter } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
 
 export const router = createHashRouter([
   {
-    path: "/",
+    path: '/',
     element: <HomePage />,
   },
-  ...profilePage,
+  {
+    path: '/user/:id',
+    element: <ProfilePage />,
+  },
 ]);
